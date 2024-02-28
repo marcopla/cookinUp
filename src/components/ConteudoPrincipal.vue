8d94c6858fa5e927dff302d53de953dec3cc75cd
Marco Pla <marco.pla@gmail.com>
2024-02-27 22:35:07 -0300

    feat(v-if/v-else) : added

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -14,9 +14,13 @@ export default{
       <span class="subtitulo-lg sua-lista-texto">
         Sua lista: 
       </span>
-      <ul class="ingredientes-sua-lista">
+      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
         <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">{{ingrediente}}</li>
       </ul>
+      <p v-else class="paragrafo lista-vazia">
+        <img src="../assets/imagens/images/icones/lista-vazia.svg" alt="icone de pesquisa">
+        Sua lista está vazia, selecione ingredientes para iniciar. 
+      </p>
     </section>
   </main>
 </template>
