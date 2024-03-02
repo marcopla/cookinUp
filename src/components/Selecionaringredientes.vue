071828a846fdc8e4c73b2b0ecc0cad9400ac7a00
Marco Pla <marco.pla@gmail.com>
2024-03-01 23:32:58 -0300

    feat(Selecionaringredientes.vue) : new component added

--- src/components/Selecionaringredientes.vue
+++ src/components/Selecionaringredientes.vue
@@ -1,7 +1,24 @@
 <script lang="ts">
-  export default{}
+import {obterCategorias} from '@/http/index'
+  export default{
+    data(){
+      return{
+        categorias: obterCategorias()
+      }
+    }
+  }
 </script>
 
 <template>
-  Selecionar
+  <section class="selecionar-ingredientes">
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+    <p class="paragrafo-lg instrucoes">Selecione abaixo os ingredientes que você quer usar nesta receita:</p>
+    <ul class="categorias">
+      <li v-for="categoria in categorias" :key="categoria.nome">
+        {{ categoria.nome }}
+      </li>
+    </ul>
+    <p class="paragrafo dica"></p>
+    *Atenção: consideramos que você tem em casa sal, pimenta e água.
+  </section>
 </template>
